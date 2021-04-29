ba0360ff8eda1f5836a0517f2ca67421ce5d6a3b
Joe Nibali <joenibali27@gmail.com>
2021-04-28 17:01:23 -0700

    Update test_question1.py

--- test_question1.py
+++ test_question1.py
@@ -4,6 +4,8 @@ import question1
 class testCaseAdd(unittest.TestCase):
     def test_volume(self):
         self.assertEqual(question1.cube(3),27)
+        self.assertEqual(question1.cube(4),64)
+        self.assertEqual(question1.cube(5),125)
 
 
 if __name__ == '__main__':
